38abb1064b6c4494b6bb629489c660ac6def95f1
shivanivdamodar <svdamodar@gmail.com>
2022-12-13 00:45:10 +0530

    extent report added

--- src/test/java/cucumber/Options/TestRunner.java
+++ src/test/java/cucumber/Options/TestRunner.java
@@ -7,7 +7,7 @@ import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
 
-@CucumberOptions(features="src/test/java/features/usersAPI.feature", glue= {"stepDefinations"})
+@CucumberOptions(features="src/test/java/features/usersAPI.feature", glue= {"stepDefinations"}, plugin= {"html:target/cucumber-reports/cucumber-html-report.html","com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"})
 public class TestRunner {
 	
 	
